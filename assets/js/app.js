eb87389fe59501cad59059cde6e0e0bf93569b91
Alexandre Balmes <alexandre@vanoix.com>
2020-09-28 13:51:25 +0200

    Fix javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,28 @@
 import '../css/app.css';
 require('mailgo/dist/mailgo.min.js');
 
-let main = document.querySelector('#main-menu');
-let list = document.querySelector('#responsive-list');
-let closeButton = document.querySelector('#close-button');
+let main = document.getElementById('main-menu');
+let list = document.getElementById('responsive-list');
+let closeButton = document.getElementById('close-button');
 
-let closeaction = ['opacity-0', 'scale-95', 'hidden'];
-let openaction = ['opacitny-100', 'scale-100', 'shown'];
+let closeAction = ['opacity-0', 'scale-95', 'hidden'];
+let openAction = ['opacity-100', 'scale-100', 'shown'];
 
-main.addEventListener('click', show(list, closeaction, openaction), false);
-closeButton.addEventListener('click', hide(list, closeaction, openaction), false);
-
-list.classList.add('opacity-0', 'scale-95', 'hidden');
-
-function hide(target, close, open) {
-  if(target.classList.contains('shown')) {
-    target.classList.remove(...open);
-    target.classList.add(...close);
+function hide() {
+  if(list.classList.contains('shown')) {
+    list.classList.remove(...openAction);
+    list.classList.add(...closeAction);
   }
 }
 
-function show(target, close, open) {
-  if(target.classList.contains('hidden')) {
-    target.classList.remove(...close);
-    target.classList.add(...open);
+function show() {
+  if(list.classList.contains('hidden')) {
+    list.classList.remove(...closeAction);
+    list.classList.add(...openAction);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  main.onclick = function() { show(); }
+  closeButton.onclick = function() { hide(); }
+});
